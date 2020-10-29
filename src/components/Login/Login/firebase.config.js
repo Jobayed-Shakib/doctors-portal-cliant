382c0044c2aaeddb5f6319a9fc7e09486ee05492
Jobayed-Shakib <71041987+Jobayed-Shakib@users.noreply.github.com>
2020-10-30 04:09:10 +0600

    Initial commit

---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDNGJAkI8CBm343DdQ7mcXmAbWaspp0d-Y",
+  authDomain: "doctors-portal-e0de9.firebaseapp.com",
+  databaseURL: "https://doctors-portal-e0de9.firebaseio.com",
+  projectId: "doctors-portal-e0de9",
+  storageBucket: "doctors-portal-e0de9.appspot.com",
+  messagingSenderId: "306052579336",
+  appId: "1:306052579336:web:a1b4db2b92fe87f6f9d1bd"
+};
+export default firebaseConfig;
